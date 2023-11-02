3c1cefc5c33401de7da4610c576b8c48c051f233
OndCac <ondrej.cacek@gym-tisnov.cz>
2023-11-02 20:19:27 +0100

    připravená první fáze k otestování

--- php/index.php
+++ php/index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Hello world</title>
+</head>
+<body>
 <?php
-echo "Hello Tour de App";
+echo("Hello TdA");
 ?>
+</body>
+</html>
