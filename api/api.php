3201645130e3af741d84d028509a17330c8fdab6
OndCac <ondrej.cacek@gym-tisnov.cz>
2023-11-05 20:49:46 +0100

    api

--- api/api.php
+++ api/api.php
@@ -2,13 +2,15 @@
 
 // Set header for JSON content
 header('Content-Type: application/json');
-  
+
+/*
 // Read JSON file
 $jsonData = file_get_contents('api.json');
 
 // Decode JSON data to PHP associative array
 $data = json_decode($jsonData, true);
+*/
 
 // Output the JSON data
-echo json_encode($data);
+echo '{"secret":"The cake is a lie"}';
 ?>
