979d35b0d66907bf48f182cdfca32ed3d0ea0319
OndCac <ondrej.cacek@gym-tisnov.cz>
2023-11-06 15:29:39 +0100

    api

--- php/api/api.php
+++ php/api/api.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: application/json'); // Ensure this is the very first thing in your file.
 
-$api = '{"secret":"The cake is a lie"}';
 // Rest of your code
-echo $api;
+echo '{"secret":"The cake is a lie"}';
