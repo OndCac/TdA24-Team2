dbd2f31f1647e3cac2f1e9795da02c4a4a9b677f
OndCac <ondrej.cacek@gym-tisnov.cz>
2023-11-06 15:20:48 +0100

    api

--- php/api/api.php
+++ php/api/api.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: application/json'); // Ensure this is the very first thing in your file.
+header('Content-Type: application/json; charset=utf-8'); // Ensure this is the very first thing in your file.
 
 // Rest of your code
 echo '{"secret":"The cake is a lie"}';
