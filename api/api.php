ed74b88ea24856e09a9240f9af16750687c12a91
OndCac <ondrej.cacek@gym-tisnov.cz>
2023-11-11 15:39:46 +0100

    api

--- api/api.php
+++ api/api.php
@@ -1,14 +1,3 @@
 <?php
-function defineEventHandler($event) {
-    // Simulating an event handler in PHP
-    if ($event === 'secret') {
-        return ['secret' => 'The cake is a lie'];
-    } else {
-        return ['error' => 'Event not found'];
-    }
-}
-
-// Usage example:
-$result = defineEventHandler('secret');
 header('Content-Type: application/json');
-echo json_encode($result);
+echo '{"secret":"The cake is a lie"}';
